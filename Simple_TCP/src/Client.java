d4ac7bec954b316d668d7d25d817da03f4af74e8
Magda Wrzosek <wrzos12mag@gmail.com>
2023-09-20 10:04:34 +0200

    server and client first version

---
+++ Simple_TCP/src/Client.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    public Client() throws Exception {
+        Socket socket = new Socket("localhost",2020);
+        System.out.println("Successful connection to the server");
+
+        //I/O Buffers
+
+        BufferedReader in_socket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out_socket = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+        String message = in_socket.readLine();
+        System.out.println("Server says: " + message);
+        out_socket.println("Thanks");
+
+        socket.close();
+        System.out.println("Socket closed");
+
+    }
+
+    public static void main(String[] args) {
+        try{
+            new Client();
+        } catch (Exception e) {
+            //TODO: handle exception
+            e.printStackTrace();
+        }
+
+    }
+}
